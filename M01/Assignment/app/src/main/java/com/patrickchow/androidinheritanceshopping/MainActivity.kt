8b1ebc674b1f3f5db7642800515dbceb4a09c97c
Patrick Chow <PatrickChow0803@gmail.com>
2019-09-09 16:49:38 -0400

    Completed the project

--- M01/Assignment/app/src/main/java/com/patrickchow/androidinheritanceshopping/MainActivity.kt
+++ M01/Assignment/app/src/main/java/com/patrickchow/androidinheritanceshopping/MainActivity.kt
@@ -3,6 +3,9 @@ package com.patrickchow.androidinheritanceshopping
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,26 +13,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        createFakeData()
+        rv_products.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = ShoppingAdapter(createFakeData())
+        }
     }
 
     fun createFakeData(): MutableList<ShoppingItem>{
         var listOfProducts = mutableListOf<ShoppingItem>()
 
-        listOfProducts.add(grocery(512, "Pie", "food"))
-        listOfProducts.add(clothing(666, "Polo Shirt", "Fabric"))
+        listOfProducts.add(grocery(111, "Pie", "food"))
+        listOfProducts.add(clothing(555, "Polo Shirt", "Fabric"))
         listOfProducts.add(toy(999, "Batman Figure", "Plastic"))
 
-        listOfProducts.add(grocery(512, "Pie", "food"))
-        listOfProducts.add(clothing(666, "Polo Shirt", "Fabric"))
+        listOfProducts.add(grocery(111, "Pie", "food"))
+        listOfProducts.add(clothing(555, "Polo Shirt", "Fabric"))
         listOfProducts.add(toy(999, "Batman Figure", "Plastic"))
 
-        listOfProducts.add(grocery(512, "Pie", "food"))
-        listOfProducts.add(clothing(666, "Polo Shirt", "Fabric"))
+        listOfProducts.add(grocery(111, "Pie", "food"))
+        listOfProducts.add(clothing(555, "Polo Shirt", "Fabric"))
         listOfProducts.add(toy(999, "Batman Figure", "Plastic"))
 
-        listOfProducts.add(grocery(512, "Pie", "food"))
-        listOfProducts.add(clothing(666, "Polo Shirt", "Fabric"))
+        listOfProducts.add(grocery(111, "Pie", "food"))
+        listOfProducts.add(clothing(555, "Polo Shirt", "Fabric"))
         listOfProducts.add(toy(999, "Batman Figure", "Plastic"))
 
         var allProducts = ""
