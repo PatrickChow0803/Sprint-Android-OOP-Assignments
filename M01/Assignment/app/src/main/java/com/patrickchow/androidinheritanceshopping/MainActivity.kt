79336328b797882c87cf47e69dc3a18ffd6be579
Patrick Chow <PatrickChow0803@gmail.com>
2019-09-09 15:35:47 -0400

    Completed Step 6 Added fake data to be generated

--- M01/Assignment/app/src/main/java/com/patrickchow/androidinheritanceshopping/MainActivity.kt
+++ M01/Assignment/app/src/main/java/com/patrickchow/androidinheritanceshopping/MainActivity.kt
@@ -2,11 +2,43 @@ package com.patrickchow.androidinheritanceshopping
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createFakeData()
+    }
+
+    fun createFakeData(): MutableList<ShoppingItem>{
+        var listOfProducts = mutableListOf<ShoppingItem>()
+
+        listOfProducts.add(grocery(512, "pie", "food"))
+        listOfProducts.add(clothing(666, "Polo Shirt", "Fabric"))
+        listOfProducts.add(toy(999, "Batman Figure", "Plastic"))
+
+        listOfProducts.add(grocery(512, "pie", "food"))
+        listOfProducts.add(clothing(666, "Polo Shirt", "Fabric"))
+        listOfProducts.add(toy(999, "Batman Figure", "Plastic"))
+
+        listOfProducts.add(grocery(512, "pie", "food"))
+        listOfProducts.add(clothing(666, "Polo Shirt", "Fabric"))
+        listOfProducts.add(toy(999, "Batman Figure", "Plastic"))
+
+        listOfProducts.add(grocery(512, "pie", "food"))
+        listOfProducts.add(clothing(666, "Polo Shirt", "Fabric"))
+        listOfProducts.add(toy(999, "Batman Figure", "Plastic"))
+
+        var allProducts = ""
+
+        //Display all the products
+        listOfProducts.forEach {
+            allProducts += "${it.product}, "
+        }
+        Log.i(allProducts, "Testing")
+        return listOfProducts
     }
 }
