ec975458dedc7b2cdf6588398baf3ac30b632331
Patrick Chow <PatrickChow0803@gmail.com>
2019-09-10 17:22:47 -0400

    Completed Step 3.4 Added an object that has an arrayList of Vehicles

---
+++ M02/Assignment/app/src/main/java/com/patrickchow/interfaceassignment/model/VehicleArrayList.kt
@@ -0,0 +1,15 @@
+package com.patrickchow.interfaceassignment.model
+
+import com.patrickchow.interfaceassignment.*
+
+object VehicleArrayList {
+
+    val vehicleArrayList = arrayListOf<Vehicle>(
+        Boat("Titanic"),
+        Plane("Jet"),
+        Car("Honda"),
+        Bus("B-31"),
+        Train("M Train"),
+        Helicopter("AH-64")
+    )
+}
