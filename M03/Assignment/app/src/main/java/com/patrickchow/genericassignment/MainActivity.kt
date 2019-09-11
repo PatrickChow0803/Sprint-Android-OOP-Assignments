1d2afc27cff83ed7e6d4c498986feb91d44d3b27
Patrick Chow <PatrickChow0803@gmail.com>
2019-09-11 15:48:49 -0400

    Completed Step 2 Create a class and try to pass in an value that isn't expected. This Doesn't work.

--- M03/Assignment/app/src/main/java/com/patrickchow/genericassignment/MainActivity.kt
+++ M03/Assignment/app/src/main/java/com/patrickchow/genericassignment/MainActivity.kt
@@ -9,8 +9,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Task 1 - Create a list of strings and try to add an Int
         var mutableListOfStrings = mutableListOf<String>("one","two","three")
-        //Compiler does not like this.
+        //Compiler doesn't like this.
         //mutableListOfStrings.add(1)
+
+        // Task 2 - create a class and pass in an object that isn't of the expected type.
+        //Compiler doesn't like this
+        //testingClass("Test")
     }
 }
+
+class testingClass(testInput: Int)
