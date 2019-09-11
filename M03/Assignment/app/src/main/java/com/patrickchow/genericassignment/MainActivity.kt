cb8adc0905ed34253579f97979659c0d0ce1c38f
Patrick Chow <PatrickChow0803@gmail.com>
2019-09-11 15:32:43 -0400

    Completed Step 1 Trying to add an Int to a list of String doesn't work.

--- M03/Assignment/app/src/main/java/com/patrickchow/genericassignment/MainActivity.kt
+++ M03/Assignment/app/src/main/java/com/patrickchow/genericassignment/MainActivity.kt
@@ -8,5 +8,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var mutableListOfStrings = mutableListOf<String>("one","two","three")
+        //Compiler does not like this.
+        //mutableListOfStrings.add(1)
     }
 }
